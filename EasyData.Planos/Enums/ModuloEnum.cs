1dcc9f9a923a08a098ffb35c3008815ea029a4e6
David Freire <davidcarlosfreire@hotmail.com>
2026-08-07 11:26:51 -0300

    - Adicionado modulo CIOT

--- EasyData.Planos/Enums/ModuloEnum.cs
+++ EasyData.Planos/Enums/ModuloEnum.cs
@@ -213,6 +213,9 @@ namespace EasyData.Planos.Enums
         [Modulo(PlanoEnum.Complementos, "Auditoria Tributária")]
         AuditoriaTributaria = 12,
 
+        [Modulo(PlanoEnum.Complementos, "CIOT")]
+        CIOT = 13,
+
         #endregion
 
 
